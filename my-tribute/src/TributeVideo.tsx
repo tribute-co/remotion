a016c745ef40be151072fd998b35c131927cc7f8
Pedro Mourelle <pedromagnus@invera.com.ar>
2021-05-26 09:17:47 -0300

    Added tracks (WIP). Improved titles styling and positioning. Added keys. Other tweaks.

--- my-tribute/src/TributeVideo.tsx
+++ my-tribute/src/TributeVideo.tsx
@@ -1,76 +1,175 @@
-import { BlockList } from 'net';
 import React from 'react';
-import { Composition, useVideoConfig, Sequence, Video } from "remotion";
-import tribute_data from './long_tribute.json';
+import {
+	Audio,
+	Composition,
+	Sequence,
+	Video,
+	spring,
+	useCurrentFrame,
+	useVideoConfig,
+} from "remotion";
+
+import tribute_data from './tribute.json';
+// import tribute_data from './long_tribute.json';
+
+export let totalFrames = 1300;  // total number of frames
+
+
+const springTransformAnim = () => {
+  const frame = useCurrentFrame();
+  const {fps} = useVideoConfig();
+
+  const scale = spring({
+    fps,
+    from: 0,
+    to: 1,
+    frame
+  });
+
+  return `scale(${scale})`
+}
 
 
 export const TributeVideo: React.FC = () => {
-	const { width, height, fps, durationInFrames } = useVideoConfig();
+	const {
+		width,
+		height,
+		fps,
+		durationInFrames,
+  } = useVideoConfig();
 
-	const { media, playlist } = tribute_data;
-	let ordered_playlist = [];
+	const { media, playlist, tracks } = tribute_data;
 
-	let current_frame = 0;  // frame to cut each divider
-	let duration:number = 0;  // duration (in frames) for each piece
+	let ordered_playlist = [];  // elements sequence
+	let audio_tracks = [];      // audio tracks
+	let current_frame = 0;      // frame to cut each divider
+	let duration:number = 0;    // duration (in frames) for each piece
+	let contentDuration:number = 0;  // duration (in frames) for media content (no audio tracks)
 
+	// get tracks
+	for (let idx=0; idx < tracks.length; idx++) {
+		debugger;
+		const {
+			// start,  // always 0, not used
+			// startOffset,
+			// volume,
+			duration,
+			originalDuration,
+			// end,    // always 0, not used
+			src,
+		} = tracks[idx];
+		audio_tracks.push(
+			<Audio
+				key={`audio#${idx}`}
+				src={src}
+				startFrom={0}
+				// startFrom={start * fps}
+				endAt={parseInt(duration * fps)}
+				// endAt={end * fps}
+			/>
+		);
+	}
+
+	// get media sequences
 	for (let idx=0; idx < playlist.length; idx++) {
-		console.log("Frame: ", current_frame);
-		console.log("Duration: ", duration);
+		// console.log("Frame: ", current_frame);
 		const sequence_data = media[playlist[idx]];
-		duration = parseInt(sequence_data.duration * fps);
+		const elemDuration = parseInt(sequence_data.duration * fps);
+		totalFrames = totalFrames + elemDuration;
 
 		switch (sequence_data.type) {
 			case 'title':
 				const { background, textFields} = sequence_data;
-				ordered_playlist.push(<Sequence from={current_frame} durationInFrames={duration}>
-					{textFields && textFields.map(data => (
-						<span
-						style={{
-							fontSize: data.fontSize + "px",
-							backgroundColor: background,
-							color: data.fontColor,
-							fontFamily: data.fontFamily,
-							fontWeight: data.fontWeight,
-							display: 'block',
-							padding: '1em',
-							position: 'absolute',
-							textAlign: 'center',
-							width: data.width * width,
-							height: data.height * height,
-							top: data.y * width,
-							left: data.x * height,
-						}}
-						>{data.text}</span>
-					))}
-				</Sequence>);
+				ordered_playlist.push(
+					<Sequence
+						from={current_frame}
+						key={`seq#${idx}`}
+						durationInFrames={elemDuration}
+					>
+						{textFields.map((data:object, idx:number) => {
+							debugger;
+							const padding = parseInt(data.fontSize);
+							const boxPadding = `${padding}px`;
+							const boxWidth = (data.width * width) - (padding * 2);
+							const boxHeight = (data.height * height) - (padding * 2);
+							const elemRotation = data.rotationIndex ? `rotate(${data.rotationIndex}deg)` : '';
+							return (
+								<span
+									key={`title#${idx}`}
+									style={{
+										fontSize: data.fontSize + "px",
+										backgroundColor: background,
+										boxSizing: 'content-box',
+										borderRadius: ".2em",
+										color: data.fontColor,
+										fontFamily: data.fontFamily,
+										fontWeight: data.fontWeight,
+										display: 'block',
+										padding: boxPadding,
+										position: 'absolute',
+										textAlign: 'center',
+										width: boxWidth,
+										height: boxHeight,
+										top: data.y * width,
+										left: data.x * height,
+										transform: elemRotation + " " + springTransformAnim(),
+									}}
+								>
+									{data.text}
+								</span>
+							);
+						})}
+					</Sequence>
+				);
 				break;
 
 			case 'video':
-				ordered_playlist.push(<Sequence from={current_frame} durationInFrames={duration}>
-					<Video
-						src={sequence_data.video}
-						// startFrom={59} // if video is 30fps, then it will start at 2s
-						// endAt={120} // if video is 30fps, then it will end at 4s
-						style={{height: height, width: width}}
-					/>
-				</Sequence>);
+				const [videoStart, videoEnd] = sequence_data.trim;
+				ordered_playlist.push(
+					<Sequence
+						key={`seq#${idx}`}
+						from={current_frame}
+						durationInFrames={elemDuration}
+					>
+						<Video
+							src={sequence_data.video}
+							startFrom={videoStart * fps}
+							endAt={videoEnd * fps}
+							style={{
+								height: height,
+								width: width
+							}}
+						/>
+					</Sequence>
+				);
 				break;
 
 			case 'img':
-				ordered_playlist.push(<Sequence from={current_frame} durationInFrames={duration}>
-					<img src={sequence_data.img} style={{ textAlign: 'center' }}/>
-				</Sequence>);
+				ordered_playlist.push(
+					<Sequence
+						key={`seq#${idx}`}
+						from={current_frame}
+						durationInFrames={elemDuration}
+					>
+						<img
+							src={sequence_data.img}
+							style={{ textAlign: 'center' }} />
+					</Sequence>
+				);
 				break;
 
 			default:
 				console.error("Unknown type to render!")
 		}
-		current_frame = current_frame + duration;
+		// update the current frame for next sequence elem
+		current_frame = current_frame + elemDuration;
 	};
-	console.log(fps);
+
+	console.log(`Total # frames: ${totalFrames}`);
 
   return (
 		<div style={{flex:1, backgroundColor: 'black'}}>
+			{audio_tracks}
 			{ordered_playlist}
 		</div>
   );

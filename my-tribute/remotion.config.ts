1eb49caa8a3ef0a074df1064cf21c39afd57d355
Pedro Mourelle <pedromagnus@invera.com.ar>
2021-07-15 12:21:42 -0300

    Added support for scss

--- my-tribute/remotion.config.ts
+++ my-tribute/remotion.config.ts
@@ -1,3 +1,29 @@
 import {Config} from 'remotion';
 
+Config.Preview.setMaxTimelineTracks(120);
 Config.Rendering.setImageFormat('jpeg');
+
+Config.Bundling.overrideWebpackConfig((currentConfiguration) => {
+  return {
+    ...currentConfiguration,
+    module: {
+      ...currentConfiguration.module,
+      rules: [
+        ...(currentConfiguration.module?.rules
+          ? currentConfiguration.module.rules
+          : []),
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+  };
+});

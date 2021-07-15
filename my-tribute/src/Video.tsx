432170b984709dde22bce34df0cff950b58495af
Pedro Mourelle <pedromagnus@invera.com.ar>
2021-07-15 12:22:43 -0300

    Getting fps and info from processed sequences

--- my-tribute/src/Video.tsx
+++ my-tribute/src/Video.tsx
@@ -1,26 +1,39 @@
-import {Composition} from 'remotion';
+import {Composition, useVideoConfig} from 'remotion';
 import {HelloWorld} from './HelloWorld';
 import {TributeVideo} from './TributeVideo';
-import {Logo} from './HelloWorld/Logo';
-import {Subtitle} from './HelloWorld/Subtitle';
-import {Title} from './HelloWorld/Title';
+// import { TributePlayer } from './TributeVideoPlayer';
+
+import { preprocess } from './tools/preprocess';
+
+// import tributeData from './long_tribute.json';
+import tributeData from './tribute.json';
+
 
 export const RemotionVideo: React.FC = () => {
+
+	const {
+		mediaAssets,
+		totalFrames,
+	} = preprocess(tributeData);
+	// console.log(totalFrames);
+
 	return (
 		<>
 			<Composition
 				id="Tribute"
 				component={TributeVideo}
-				durationInFrames={28900}
+				durationInFrames={totalFrames}
 				fps={30}
 				width={1920}
 				height={1080}
 				defaultProps={{
-					titleText: 'Tribute video',
+					titleText: "Tribute video",
 					titleColor: 'black',
+					mediaAssets,
+					totalFrames,
 				}}
 			/>
-			<Composition
+			{/* <Composition
 				id="HelloWorld"
 				component={HelloWorld}
 				durationInFrames={150}
@@ -59,7 +72,7 @@ export const RemotionVideo: React.FC = () => {
 				fps={30}
 				width={1920}
 				height={1080}
-			/>
+			/> */}
 		</>
 	);
 };
